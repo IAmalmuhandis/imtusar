186549d08a1f413d484f5b53e4b02f84df3f8d30
IAmAlmuhandis <abubakaralmuhandis@gmail.com>
2023-12-18 16:46:48 +0100

    new changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
+import logo from './assets/imtusar.png';
 import './App.css';
+import './ComingSoon.css'; // Import the new CSS file for styling
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save tfddfso fsfreload.update
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="coming-soon-container">
+          <h1>Coming Soon</h1>
+          <div className="loader"></div>
+        </div>
       </header>
     </div>
   );
